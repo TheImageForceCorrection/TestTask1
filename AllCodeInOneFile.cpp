069a3b7cfee63e9428afa466de7131f470046e96
TheImageForceCorrection <kasyanov_vladimir@ngs.ru>
2019-02-04 11:32:25 +0700

    Update AllCodeInOneFile.cpp

--- AllCodeInOneFile.cpp
+++ AllCodeInOneFile.cpp
@@ -604,20 +604,15 @@ public:
 int main()
 {
 	try
-	
 	{
 		ChatProgram chatProgram;
 		chatProgram.execute();
 	}
-
 	catch (const ChatException &e)
-
 	{
 		cout << e;
 	}
-
 	catch (...)
-
 	{
 		cout << "System error\n";
 	}
