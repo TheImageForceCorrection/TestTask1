0b08517425c8c52da7e171d6d183e4c9e6fa5ca6
TheImageForceCorrection <kasyanov_vladimir@ngs.ru>
2019-02-04 19:33:21 +0700

    Update ChatAutomaticClients.cpp

--- src/ChatAutomaticClients.cpp
+++ src/ChatAutomaticClients.cpp
@@ -1,6 +1,6 @@
 #include "../include/ChatAutomaticClients.h"
 
-ChatAutomaticClients::ChatAutomaticClients(unsigned int _nClients, const int *_timeGaps, const string *_messageTexts, ChatServer &_chatServer)
+ChatAutomaticClients::ChatAutomaticClients(unsigned int _nClients, const int *_timeGaps, const std::string *_messageTexts, ChatServer &_chatServer)
 {
 	for (unsigned int i = 0; i < _nClients; i++)
 	{
@@ -11,17 +11,17 @@ ChatAutomaticClients::ChatAutomaticClients(unsigned int _nClients, const int *_t
 void ChatAutomaticClients::startAutomaticSending()
 {
 	for (unsigned int i = 0; i < size(); i++)
-		operator[](i)->startAutomaticSending();
+		at(i)->startAutomaticSending();
 }
 
 void ChatAutomaticClients::joinThreads()
 {
 	for (unsigned int i = 0; i < size(); i++)
-		operator[](i)->joinThread();
+		at(i)->joinThread();
 }
 
 ChatAutomaticClients::~ChatAutomaticClients()
 {
 	for (unsigned int i = 0; i < size(); i++)
-		delete operator[](i);
+		delete at(i);
 }
