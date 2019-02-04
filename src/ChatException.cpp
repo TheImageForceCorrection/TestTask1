0594371a964627a1bb9d776250a65039ccb6f2ee
TheImageForceCorrection <kasyanov_vladimir@ngs.ru>
2019-02-04 19:49:24 +0700

    Update ChatException.cpp

--- src/ChatException.cpp
+++ src/ChatException.cpp
@@ -1,6 +1,6 @@
 #include "../include/ChatException.h"
 
-ostream &operator<<(ostream &os, const ChatException &_exception)
+std::ostream &operator<<(std::ostream &os, const ChatException &_exception)
 {
 	os << _exception.what();
 	return os;
