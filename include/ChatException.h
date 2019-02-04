aa4a5eacdc4a89e1035aabbe413bcc25be974531
TheImageForceCorrection <kasyanov_vladimir@ngs.ru>
2019-02-04 19:45:12 +0700

    Update ChatException.h

--- include/ChatException.h
+++ include/ChatException.h
@@ -2,14 +2,12 @@
 
 #include <string>
 
-using namespace std;
-
 //	class ChatException is a special exception class
 class ChatException {
 
 private:
 
-	const string errDescription;
+	const std::string errDescription;
 
 	ChatException() = delete;
 
@@ -17,18 +15,18 @@ public:
 
 	//	ChatException::ChatException(const string &_errDescription, const string &_whereFunc) creates throwing object with
 	//	a description of the exception reason in _errDescription and the name of the method which has throwed the exception
-	ChatException(const string &_errDescription, const string &_whereFunc) :
+	ChatException(const std::string &_errDescription, const std::string &_whereFunc) :
 		errDescription("\nError in " + _whereFunc + "\n" + _errDescription + "\n\n")
 	{
 
 	}
 
 	//	const string &ChatException::what() const returns string containing the information about the exception reason
-	const string &what() const
+	const std::string &what() const
 	{
 		return errDescription;
 	}
 
 };
 
-ostream &operator<<(ostream &os, const ChatException &_exception);
+std::ostream &operator<<(std::ostream &os, const ChatException &_exception);
